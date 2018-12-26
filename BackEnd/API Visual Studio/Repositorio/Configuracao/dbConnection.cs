6f544faa977b34e49ea0935b6562257a16765a8c
Caléo Caciano <caleo.caciano@gmail.com>
2018-12-26 08:32:53 -0200

    .

--- BackEnd/API Visual Studio/Repositorio/Configuracao/dbConnection.cs
+++ BackEnd/API Visual Studio/Repositorio/Configuracao/dbConnection.cs
@@ -7,12 +7,12 @@ namespace Repositorio.Configuracao
 {
     public static class DbConnection
     {
-        private static string DataSource = "NB_CACIANO"; //DESKTOP-CGEHIFJ\\SQLEXPRESS
+        private static string DataSource = "WKSJUN000105"; //DESKTOP-CGEHIFJ\\SQLEXPRESS   //WKSJUN000105
         private static string Catalog = "SCOPIO";
         private static string User = "sa";
-        private static string Password = "br1108";
+        private static string code = "123Aa321";
 
-        private static string Conn1 = @"Data Source=" + DataSource + ";Initial Catalog=" + Catalog + ";Persist Security Info=True;User ID=" + User + ";Password=" + Password + ";Connect Timeout=30";
+        private static string Conn1 = @"Data Source=" + DataSource + ";Initial Catalog=" + Catalog + ";Persist Security Info=True;User ID=" + User + ";Password=" + code + ";Connect Timeout=30";
         private static string Conn2 = @"Data Source=" + DataSource + ";Initial Catalog=" + Catalog + ";Integrated Security=True;Connect Timeout=30";
 
         private static string ConnectionString = Conn1;
